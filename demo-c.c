f45425b62f2e733215e4884181485e5a62f3fd31
o-meraba <omeraba93@gmail.com>
2020-12-17 22:51:58 +0300

    added useful-info.txt file

---
+++ demo-c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/* demo.c: My first C program on a Linux */
+
+int main(void)
+{
+ printf("hello! This is a test program. \n");
+ printf("this is second line. \n");
+ return 0;
+
+}
+
